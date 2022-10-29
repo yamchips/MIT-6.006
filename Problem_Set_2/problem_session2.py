0902452ba019eea031f2cf2798d340531f0e9948
yamchips <52370991+yamchips@users.noreply.github.com>
2022-10-29 21:57:06 +0800

    Add files via upload

--- Problem_Set_2/problem_session2.py
+++ Problem_Set_2/problem_session2.py
@@ -6,59 +6,35 @@
 @Author  : yamchips
 @Version : 1.0
 """
-import numpy as np
 #Problem 1-4 Brick Blowing
 #subproblem b
 def calspecial(D):
     '''
-    Input: D is an array that only one element is not special, which 
+    Input: D is a list that only one element is not special, which 
           means D consists of two sorted arrays
-    Output: An array called 'result'. Each element indicates the damage of the wolf
+    Output: A list 'result'. Each element indicates the damage of the wolf
     '''
-    result = []
-    for j in range(len(D)):
-        result.extend([1])
+    result = [1]*len(D)
     for i in range(len(D)-1):
         if D[i] > D[i+1]:
             specialnum = i
-    i, j = 0, specialnum+1
+    i, j = 0, specialnum + 1
     while i <= specialnum and j < len(D):
         if D[i] > D[j]:
             result[i] += 1
             j += 1
         else:
             i += 1
- 
     result1 = result[:]
-    print(type(result))
     for i in range(1,specialnum+1):
-        #for j in range(len(result1)):
-        #    print(result[j])
-        #print('---------------')
-        a = result1[i] + sum(result1[:i]) - i
-        result[i] = a
-    print('result:', result)
-    #根据i和j数值不同调试一下
-    #for j in range(1,4):
-    #    result[j] = result1[j] + sum(result1[:j]) - j
-
+        result[i] = result1[i] + sum(result1[:i]) - i   
     return result
-#max(A) == max(B)
-#D = [1,2,3,4,1,2,3]
-
-#max(B) < max(A)
-D = [5,6,7,8,1,2,3,4]
-
-#MAX(B)>MAX(A)
-#D = [1,2,3,4,1,2,8,9]
-print(calspecial(D))
-
 
 #subproblem c
 def caldamage(D):
     '''
-    Input: An array D, integer
-    Output: An array, each element means the damage of the wolf
+    Input: D is an unsorted list of integer
+    Output: A list whose element means the damage of the wolf
     '''
     if len(D) <= 1:
         return D
@@ -68,3 +44,31 @@ def caldamage(D):
         right = caldamage(D[mid:])
         return calspecial(left)
 
+#standard solution
+def get_damages(H):
+    D = [1 for _ in H]
+    H2 = [(H[i], i) for i in range(len(H))]
+    def merge_sort(A, a=0, b=None):
+        if b is None:
+            b = len(A)
+        if a + 1 < b:
+            c = (a+b+1)//2
+            merge_sort(A, a, c)
+            merge_sort(A, c, b)
+            i, j, L, R = 0, 0, A[a:c], A[c:b]
+            while a < b:
+                if (j>=len(R)) or (i<len(L)) and L[i][0] <= R[j][0]:
+                    D[L[i][1]] += j
+                    A[a] = L[i]
+                    i += 1
+                else:
+                    A[a] = R[j]
+                    j += 1
+                a += 1
+    merge_sort(H2)
+    return D
+
+#H = [1,2,3,4,1,2,3,4]
+#H = [1,2,3,4,1,3,9,12]
+H = [5,6,7,8,1,2,3,4]
+print(get_damages(H))
